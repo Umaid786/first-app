bb7290813dc8b6948f098b9bced25e9072fbb3e4
Umaid786 <muhammadumaid786@gmail.com>
2022-03-11 21:51:25 +0500

    Updates

---
+++ precache-manifest.ea4b91e3370020830e94002d8501e67a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "8ff5f0504a1a87ccfc1f",
+    "url": "/first-app/static/css/main.e8681336.chunk.css"
+  },
+  {
+    "revision": "8ff5f0504a1a87ccfc1f",
+    "url": "/first-app/static/js/main.8ff5f050.chunk.js"
+  },
+  {
+    "revision": "629ee088d0c3fc952ac4",
+    "url": "/first-app/static/js/1.629ee088.chunk.js"
+  },
+  {
+    "revision": "3a2e0e0b763789a1abbb",
+    "url": "/first-app/static/js/2.3a2e0e0b.chunk.js"
+  },
+  {
+    "revision": "8fb928e1006184d3e1f5",
+    "url": "/first-app/static/js/runtime~main.8fb928e1.js"
+  },
+  {
+    "revision": "12b6e2e74ee3f55484bf74a299bf562d",
+    "url": "/first-app/index.html"
+  }
+];

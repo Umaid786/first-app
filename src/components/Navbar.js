39ed60ee522438f50c5c3389e2ff31b73dd12c0c
Umaid786 <muhammadumaid786@gmail.com>
2022-03-11 21:22:20 +0500

    Version 2 for the href check

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -6,7 +6,7 @@ export default function Navbar(props) {
     <nav className={`navbar navbar-expand-lg navbar-${props.mode} bg-${props.mode}`}>
       <div className="container-fluid">
         {/* <Link className="navbar-brand" to="/">{props.title}</Link> */}
-        <a className="navbar-brand" href="/">{props.title}</a>
+        <a className="navbar-brand" href="#">{props.title}</a>
         <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"></span>
         </button>
@@ -14,17 +14,17 @@ export default function Navbar(props) {
           <ul className="navbar-nav me-auto mb-2 mb-lg-0">
             <li className="nav-item">
               {/* <Link className="nav-link active" aria-current="page" to="/">Home</Link> */}
-              <a className="nav-link active" aria-current="page" href="/">Home</a>
+              <a className="nav-link active" aria-current="page" href="#">Home</a>
             </li>
             <li className="nav-item">
               {/* <Link className="nav-link" to="/">{props.secondTitle}</Link> */}
-              <a className="nav-link" href="/">{props.secondTitle}</a>
+              <a className="nav-link" href="#">{props.secondTitle}</a>
             </li>
             <li className="nav-item dropdown">
               {/* <Link className="nav-link dropdown-toggle" to="/" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
                 Most Popular
               </Link> */}
-              <a className="nav-link dropdown-toggle" href="/" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+              <a className="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
                 Most Popular
               </a>
               <ul className="dropdown-menu" aria-labelledby="navbarDropdown">
@@ -33,17 +33,17 @@ export default function Navbar(props) {
                 <li><Link className="dropdown-item" to="/">Technology</Link></li>
                 <li><hr className="dropdown-divider"/></li>
                 <li><Link className="dropdown-item" to="/">Sports</Link></li> */}
-                <li><a className="dropdown-item" href="/">Global</a></li>
-                <li><a className="dropdown-item" href="/">Business</a></li>
-                <li><a className="dropdown-item" href="/">Technology</a></li>
+                <li><a className="dropdown-item" href="#">Global</a></li>
+                <li><a className="dropdown-item" href="#">Business</a></li>
+                <li><a className="dropdown-item" href="#">Technology</a></li>
                 <li><hr className="dropdown-divider"/></li>
-                <li><a className="dropdown-item" href="/">Sports</a></li>
+                <li><a className="dropdown-item" href="#">Sports</a></li>
               </ul>
             </li>
             <li className="nav-item">
               {/* <Link className="nav-link" to="/about">About Climate Change</Link> */}
               {/* <a className="nav-link" href="/about">About Climate Change</a> */}
-              <a className="nav-link" href="/">About Climate Change</a>
+              <a className="nav-link" href="#">About Climate Change</a>
             </li>
           </ul>
           <form className="d-flex">
